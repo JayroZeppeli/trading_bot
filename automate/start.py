7f893887637749a93cf4128077919bae79badf12
JayroZeppeli <95538222+JayroZeppeli@users.noreply.github.com>
2022-04-27 21:02:54 +0200

    1.3.2

--- automate/start.py
+++ automate/start.py
@@ -11,10 +11,10 @@ from interaction_binance.tour_de_guet import guet
 from donnees.trader import choix_position
 
 
-def automate(deja_en_position=False):
+def automate(position=False):
     ecrit_rapport("Lancement...", initialisation=True)
     while True:
-        if deja_en_position:
+        if position:
             guet()
         attendre_prochaine_bougie(True)
         [position, sens, take_profit, stop_loss], quantite = choix_position()
@@ -23,7 +23,6 @@ def automate(deja_en_position=False):
             attendre_quelques_minutes()
             continue
         ouvrir_position(sens, take_profit, stop_loss, quantite)
-        guet()
 
 
 automate()
